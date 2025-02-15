24af88ce64687bca4c83e542e76724d4541ae695
HESHAMKAMAll <heshamkamal422@gmail.com>
2025-02-15 09:43:34 -0800

    only UI

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  // static const double kPadding = 10.0;
+  static const Color purpleLight = Color(0XFF1e224c);
+  static const Color purpleDark = Color(0XFF0d193e);
+  static const Color orange = Color(0XFFec8d2f);
+  static const Color red = Color(0XFFf44336);
+
+  static const double kPadding = 8.0;
+
+  static const Color primaryColor = Colors.orangeAccent;
+  static const Color textColor = Colors.white70;
+  static const Color darkBackground = Color(0xFF1E1E1E);
+  static const Color dividerColor = Colors.grey;
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_panel/widget_tree.dart';
+
+import 'constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Admin Panel',
+      theme: ThemeData(
+          scaffoldBackgroundColor: Constants.purpleDark,
+          primarySwatch: Colors.blue,
+          canvasColor: Constants.purpleLight),
+      home: WidgetTree(),
+    );
+  }
+}
